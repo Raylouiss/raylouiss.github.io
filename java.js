20c8003d3f0b112b81f3db204412bf5562ac668d
Raylouiss <92111319+Raylouiss@users.noreply.github.com>
2021-10-30 11:56:51 +0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,114 @@
+var halaman1 = document.getElementById("page1");
+var btn = document.getElementById("btn1");
+
+    // Get the <span> element that closes the modal
+var span = document.getElementsByClassName("tutup")[0];
+
+    // When the user clicks the button, open the modal 
+btn.onclick = function() {
+halaman1.style.display = "block";
+}
+
+    // When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+halaman1.style.display = "none";
+}
+
+    // When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+if (event.target == halaman1) {
+    halaman1.style.display = "none";
+}
+}
+
+var halaman2 = document.getElementById("page2");
+var btn = document.getElementById("btn2");
+
+    // Get the <span> element that closes the modal
+var span = document.getElementsByClassName("tutup2")[0];
+
+    // When the user clicks the button, open the modal 
+btn.onclick = function() {
+halaman2.style.display = "block";
+}
+
+    // When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+halaman2.style.display = "none";
+}
+
+    // When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+if (event.target == halaman2) {
+    halaman2.style.display = "none";
+}
+}
+
+var halaman3 = document.getElementById("page3");
+var btn = document.getElementById("btn3");
+
+    // Get the <span> element that closes the modal
+var span = document.getElementsByClassName("tutup3")[0];
+
+    // When the user clicks the button, open the modal 
+btn.onclick = function() {
+halaman3.style.display = "block";
+}
+
+    // When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+halaman3.style.display = "none";
+}
+
+    // When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+if (event.target == halaman3) {
+    halaman3.style.display = "none";
+}
+}
+
+var halaman4 = document.getElementById("page4");
+var btn = document.getElementById("btn4");
+
+    // Get the <span> element that closes the modal
+var span = document.getElementsByClassName("tutup4")[0];
+
+    // When the user clicks the button, open the modal 
+btn.onclick = function() {
+halaman4.style.display = "block";
+}
+
+    // When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+halaman4.style.display = "none";
+}
+
+    // When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+if (event.target == halaman4) {
+    halaman4.style.display = "none";
+}
+}
+
+var halaman5 = document.getElementById("page5");
+var btn = document.getElementById("btn5");
+
+    // Get the <span> element that closes the modal
+var span = document.getElementsByClassName("tutup5")[0];
+
+    // When the user clicks the button, open the modal 
+btn.onclick = function() {
+halaman5.style.display = "block";
+}
+
+    // When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+halaman5.style.display = "none";
+}
+
+    // When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+if (event.target == halaman5) {
+    halaman5.style.display = "none";
+}
+}
